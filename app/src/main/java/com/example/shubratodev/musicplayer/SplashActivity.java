26b52a5112f053c458ef18c3d4679ce940604b74
Leonious-Shuvo <shuvodev1992@gmail.com>
2018-10-22 01:35:03 +0600

    music player

---
+++ app/src/main/java/com/example/shubratodev/musicplayer/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.example.shubratodev.musicplayer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ProgressBar prg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        prg =(ProgressBar) findViewById(R.id.progressBar);
+        prg.setMax(3);
+        prg.setProgress(0);
+        final Thread myThread = new Thread()
+        {
+            @Override
+            public void run() {
+                try {
+                    for (int i=0;i<3;i++)
+                    {
+                        prg.setProgress(i);
+                        sleep(3000);
+                    }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
